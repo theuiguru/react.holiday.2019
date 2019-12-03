da4821660e741a0b7cd294a964bc33726c88ad4f
Chris Thomas <webmaster102@gmail.com>
2019-12-03 10:21:48 -0500

    Destructuring assignments and JSX spread attributes

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function Friends(props) {
-  return <h2>{props.name}</h2>;
+function Friends({ name, ...props }) {
+  return <h2 {...props}>{name}</h2>;
 }
 
 function App() {
   return (
     <div>
       <h1>My Friends</h1>
-      <Friends name="Robin" />
-      <Friends name="Shajan" />
-      <Friends name="Noel" />
+      <Friends name="Robin" class="friend" />
+      <Friends name="Shajan" id="2" />
+      <Friends name="Noel" data-something="something" />
     </div>
   );
 }
