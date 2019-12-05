ec536d4a8514fd09ff28ca5544659072248f3557
Chris Thomas <webmaster102@gmail.com>
2019-12-04 23:07:04 -0500

    Tracked state with React.useState()

--- src/index.js
+++ src/index.js
@@ -3,17 +3,25 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const collection = [
+  {name: "Robin"},
+  {name: "Christian"},
+  {name: "Shajan"},
+  {name: "Noel"}
+];
+
 function Friends({ name, ...props }) {
   return <h2 {...props}>{name}</h2>;
 }
 
 function App() {
+  let [index, setIndex] = React.useState(0);
   return (
     <div>
-      <h1>My Friends</h1>
-      <Friends name="Robin" class="friend" />
-      <Friends name="Shajan" id="2" />
-      <Friends name="Noel" data-something="something" />
+      <button type="button" onClick={() => setIndex(index + 1)}>
+        Next
+      </button>
+      <Friends name={collection[index].name} />
     </div>
   );
 }
